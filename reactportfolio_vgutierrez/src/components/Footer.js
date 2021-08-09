95c1c0823d8c4997e569e856f1a9eb8cd15c39c9
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-08-08 21:20:08 -0700

    Main framework for portfolio

---
+++ reactportfolio_vgutierrez/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+// Here we are importing a CSS file as a dependency
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h1>Home</h1>
+    </footer>
+  );
+}
+
+export default Footer;
